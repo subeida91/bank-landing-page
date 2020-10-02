4e45628fb2f8f74e2fd28a8bfabd15aef6ea10cb
subeida91 <subeida91@gmail.com>
2020-10-02 18:45:24 +0100

    added animation for the hamburger menu

--- app/dist/js/script.js
+++ app/dist/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello")
+const btnhamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector('.overlay');
+
+btnhamburger.addEventListener('click', function(){
+    console.log("click hamburger");
+
+    if(header.classList.contains('open')){ //close hamburger menu
+        header.classList.remove('open');
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+        
+
+        
+    }
+    else{ //open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove("fade-out")
+        overlay.classList.add("fade-in");
+        
+    }
+    
+});
