72ca7317ee1c654a7a24b4797b33807db0a25cf0
subeida91 <subeida91@gmail.com>
2020-10-15 18:27:51 +0100

    started the hero section

--- app/dist/js/script.js
+++ app/dist/js/script.js
@@ -1,23 +1,28 @@
 const btnhamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector("body");
 
-btnhamburger.addEventListener('click', function(){
-    console.log("click hamburger");
+let navigationAnimation = function(){
 
-    if(header.classList.contains('open')){ //close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+if(header.classList.contains('open')){ //close hamburger menu
+  header.classList.remove("open");
+  fadeElems.forEach(function(element){
+      body.classList.remove('noscroll');
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+  });
+}
+else { //open hamburger menu
+    header.classList.add("open");
+    fadeElems.forEach(function(element){
+        body.classList.add('noscroll');
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
         
-
-        
-    }
-    else{ //open hamburger menu
-        header.classList.add('open');
-        overlay.classList.remove("fade-out")
-        overlay.classList.add("fade-in");
-        
-    }
-    
 });
+};
+}
+
+btnhamburger.onclick = navigationAnimation;
